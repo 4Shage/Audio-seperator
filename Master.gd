e361d1b167b940fd1922288e4e20cd14e5bee702
4Shage <47282189+4Shage@users.noreply.github.com>
2024-12-28 10:47:53 +0100

    Bug fix
    Repaired bug that prvented from exporting

--- Master.gd
+++ Master.gd
@@ -30,7 +30,7 @@ func _on_pressed() -> void:
 		print("No file selected")
 		return
 	else: print(cfg.get_val("file_path"))
-	command = "cd "+(cfg.get_val("file_path").replace(cfg.get_val("file_name"),""))+";"+"ffmpeg -i " + "'./"+cfg.get_val("file_name")+"'"
+	command = "cd "+"\'"+(cfg.get_val("file_path").replace(cfg.get_val("file_name"),""))+"\'"+";"+"ffmpeg -i " + "'./"+cfg.get_val("file_name")+"'"
 	if cfg.has_val("audio_tracks"):
 		for e: String in cfg.get_val("audio_tracks"):
 			command = command + " " + "-map 0:a:" + str(cfg.get_val("audio_tracks")[e]) + " "+(cfg.get_val("file_name").replace(cfg.get_val("file_name").get_extension(), "")).replace(" ", "-") + "-" + e + ".aac"
@@ -49,6 +49,7 @@ func _process(_delta: float) -> void:
 func seperate_tracks() -> Array:
 	var output: Array
 	# Execute the command in PowerShell
+	print(command)
 	var result = OS.execute("powershell.exe", ["-Command",command], output,true, true)
 	
 	if result == OK:
